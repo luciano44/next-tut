945c178b6ffecf23e2b7511f4e12cde1cca33f5c
Luciano Ribeiro <luciano.ar6@gmail.com>
2023-11-26 23:28:35 -0300

    setup navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -13,17 +13,9 @@ const navLinks = [
 const Navbar = () => {
   return (
     <nav>
-      {/* <Link href="/">Home - </Link>
-      <Link href="/about">About - </Link>
-      <Link href="/client">Client - </Link>
-      <Link href="/drinks">Drinks - </Link>
-      <Link href="/prisma-example">Prisma - </Link>
-      <Link href="/query">Query - </Link>
-      <Link href="/tasks">Tasks</Link> */}
-
       {navLinks.map((nl) => (
-        <Link key={nl[0]} href={nl[0]}>
-          {nl[1]} - &nbsp;
+        <Link key={nl[0]} href={nl[0]} className="mr-4">
+          {nl[1]}
         </Link>
       ))}
     </nav>
