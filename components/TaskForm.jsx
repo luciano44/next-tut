56b10629546e79207ebd112cc101e366ac3f4c7c
Luciano Ribeiro <luciano.ar6@gmail.com>
2023-12-02 23:56:40 -0300

    create first server component (taskForm)

--- components/TaskForm.jsx
+++ components/TaskForm.jsx
@@ -1,4 +1,25 @@
+const createTask = async (formData) => {
+  "use server";
+  const content = formData.get("content");
+  console.log(content);
+};
+
 const taskForm = () => {
-  return <form>TaskForm</form>;
+  return (
+    <form action={createTask}>
+      <div className="join w-full">
+        <input
+          type="text"
+          placeholder="type here"
+          className="input input-bordered join-item w-full"
+          name="content"
+          required
+        />
+        <button type="submit" className="btn btn-primary join-item">
+          create task
+        </button>
+      </div>
+    </form>
+  );
 };
 export default taskForm;
